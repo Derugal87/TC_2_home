c165ad5639b57c596d3646a0ebe2ef15861c7ea9
Alexander Derugo <60985658+Derugal87@users.noreply.github.com>
2022-12-08 16:16:16 +0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -5,11 +6,10 @@ module.exports = {
   solidity: "0.8.9",
   networks: {
     testnet: {
-      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      url: "https://bsctestapi.terminet.io/rpc",
       chainId: 97,
       accounts: {
-        mnemonic:
-          "average sunset brick render holiday purpose another despair almost ignore decide ask",
+        mnemonic: process.env.MNEMONIC,
       },
     },
   },
