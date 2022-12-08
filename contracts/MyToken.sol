70b7c1c48fd4c5bf2f1b24c59f46056f429417eb
Alexander Derugo <60985658+Derugal87@users.noreply.github.com>
2022-12-08 16:15:20 +0400

    Update MyToken.sol

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,7 +8,7 @@ contract MyToken {
 
     mapping(address => uint) public _balanceOf;
     mapping(address => mapping(address => uint)) public _allowance;
-    mapping(address => bool) public _blacklist;
+    mapping(address => bool) private _blacklist;
 
     string public _name = "NewToken";
     string public _symbol = "NT";
@@ -17,13 +17,15 @@ contract MyToken {
     event Transfer(address indexed from, address indexed to, uint amount);
     event Approve(address indexed owner, address indexed spender, uint amount);
 
-    constructor() {
-        owner = msg.sender;
-    }
 
-    modifier OnlyOwner() {
+    modifier onlyOwner() {
         require(msg.sender == owner, "only owner");
         _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        mint();
     }
 
     function name() public view returns (string memory) {
@@ -36,48 +38,52 @@ contract MyToken {
         return _decimals;
     }
 
-    function to_blacklist(address _address) external OnlyOwner {
+    function to_blacklist(address _address) public onlyOwner {
         require(!_blacklist[_address], "this address is now blacklisted");
         _blacklist[_address] = true;
     }
 
-    function totalSupply() external view returns(uint) {
+    function totalSupply() public view returns(uint) {
         return _totalSupply;
     }
 
-    function balanceOf(address account) external view returns(uint) {
+    function balanceOf(address account) public view returns(uint) {
         return _balanceOf[account];
     }
 
-    function transfer(address recipient, uint amount) external returns(bool) {
-        require(!_blacklist[recipient], "already blacklisted");
+    function transfer(address recipient, uint amount) public returns(bool) {
+        require(!_blacklist[msg.sender], "already blacklisted");
+        require(_balanceOf[msg.sender] >= amount, "unsufficient balance");
         _balanceOf[msg.sender] -= amount;
         _balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function allowance(address _owner, address spender) external view returns(uint) {
-        return _allowance[_owner][spender];
-    }
-
-    function mint() external OnlyOwner { 
-        uint amount = 500000000000000000000;
-        _balanceOf[msg.sender] += amount;
-        _totalSupply += amount;
-    }    
-    
-    function approve(address spender, uint amount) external returns(bool) {
-        _allowance[msg.sender][spender] = amount;
-        emit Approve(msg.sender, spender, amount);
-        return true;
-    }
-
-    function transferFrom(address sender, address recipient, uint amount) external returns(bool) {
+    function transferFrom(address sender, address recipient, uint amount) public returns(bool) {
+        require(!_blacklist[msg.sender], "already blacklisted");
+        require(_balanceOf[sender] >= amount, "unsufficient balance");
         _allowance[sender][recipient] -= amount;
         _balanceOf[sender] -= amount;
         _balanceOf[recipient] += amount;
         emit Transfer(sender, recipient, amount);
         return true;
     }
+
+    function allowance(address _owner, address spender) public view returns(uint) {
+        return _allowance[_owner][spender];
+    }
+
+    function mint() public {
+        require(!_blacklist[msg.sender], "already blacklisted");
+        _balanceOf[msg.sender] = 500e18;
+    }  
+    
+    function approve(address spender, uint amount) public returns(bool) {
+        _allowance[msg.sender][spender] = amount;
+        emit Approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    
 }
